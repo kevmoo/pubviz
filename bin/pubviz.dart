42b1966a1b8803183097472fcd121a301ebaf55c
Kevin Moore <kevin@thinkpixellab.com>
2013-07-23 19:20:14 -0400

    handle running pubviz within a package directory without arguments
    if it's a valid project, it should just work

--- bin/pubviz.dart
+++ bin/pubviz.dart
@@ -1,21 +1,28 @@
 #!/usr/bin/env dart
 
 import 'dart:io';
+import 'package:path/path.dart' as pathos;
 import 'package:pubviz/pubviz.dart';
 
 void main() {
-  var args = (new Options()).arguments;
 
-  if(args.length != 1) {
-    print("should only have one arg -> a path");
-    exit(1);
-    return;
-  }
-
-  final path = args[0];
+  final path = _getPath();
 
   VizRoot.forDirectory(path)
     .then((VizRoot vp) {
       print(vp.toDot());
     });
+}
+
+String _getPath() {
+  var args = (new Options()).arguments;
+
+  if(args.isEmpty) {
+    return pathos.current;
+  }
+
+  if(args.length != 1) {
+    print("should only have one arg -> a path");
+    exit(1);
+  }
 }
