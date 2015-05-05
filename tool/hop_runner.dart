024d829c4242fbda15a460a47bd6290d5aba9307
Kevin Moore <kevmoo@google.com>
2015-05-05 09:28:24 -0700

    remove hop

--- tool/hop_runner.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hop/hop.dart';
-import 'package:hop_git/hop_git.dart';
-
-void main(List<String> args) {
-  addTask('pages', (ctx) => branchForDir(ctx, 'master', 'example', 'gh-pages'));
-
-  runHop(args);
-}
