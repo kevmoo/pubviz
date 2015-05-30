f7e95f7460398c31fd4ac0f1aeaa76e843139474
Kevin Moore <kevmoo@google.com>
2015-05-30 10:30:00 -0700

    tiny tweak to travis script

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Fast fail the script on failures.   
+# Fast fail the script on failures.
 set -e
 
 dart --checked test/viz_test.dart
@@ -8,8 +8,6 @@ dart --checked test/viz_test.dart
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
-    --retry 2 \
     --exclude-test-files \
     test/viz_test.dart
 fi
