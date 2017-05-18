e1e85c589dd096d20cddb17c1ef4059ec8676db7
Kevin Moore <kevmoo@google.com>
2017-05-18 16:39:22 -0700

    Provide a descriptive user agent to the server

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:pub_semver/pub_semver.dart';
 
 Future<Version> getLatestVersion(String packageName) async {
-  var response =
-      await http.get("https://pub.dartlang.org/packages/$packageName.json");
+  var response = await http
+      .get("https://pub.dartlang.org/packages/$packageName.json", headers: {
+    HttpHeaders.USER_AGENT: "pubviz - Dart ${Platform.version.split(" ").first}"
+  });
 
   if (response.statusCode != 200) {
     return null;
