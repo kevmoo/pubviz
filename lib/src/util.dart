e8fa4169157c9e3dd1296581657dc5379bd37f69
Kevin Moore <kevmoo@google.com>
2017-05-18 17:51:16 -0700

    Adding retry logic to HTTP requests

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -6,10 +6,29 @@ import 'package:http/http.dart' as http;
 import 'package:pub_semver/pub_semver.dart';
 
 Future<Version> getLatestVersion(String packageName) async {
-  var response = await http
-      .get("https://pub.dartlang.org/packages/$packageName.json", headers: {
-    HttpHeaders.USER_AGENT: "pubviz - Dart ${Platform.version.split(" ").first}"
-  });
+  http.Response response;
+
+  var retries = 0;
+  while (true) {
+    var path = "https://pub.dartlang.org/packages/$packageName.json";
+    try {
+      response = await http.get(path, headers: {
+        HttpHeaders.USER_AGENT: "pubviz - Dart ${Platform.version
+            .split(" ")
+            .first}"
+      });
+      break;
+    } catch (e) {
+      stderr.writeln(e);
+
+      retries++;
+      if (retries > 3) {
+        rethrow;
+      } else {
+        stderr.writeln('Retrying `$path`.');
+      }
+    }
+  }
 
   if (response.statusCode != 200) {
     return null;
