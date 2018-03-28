a67fe3d6e6713cc4a03e9eb5222bd6d89be1abdd
Kevin Moore <kevmoo@google.com>
2018-03-27 20:43:41 -0700

    Use test_descriptor

--- test/viz_test.dart
+++ test/viz_test.dart
@@ -5,29 +5,21 @@ import 'package:pub_semver/pub_semver.dart';
 import 'package:pubviz/pubviz.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
+import 'package:test_descriptor/test_descriptor.dart' as d;
 
-Directory _tempDir;
-
-// TODO(kevmoo): use pkg:test_descriptor
 void main() {
   setUp(() async {
     await _initTest();
   });
 
-  tearDown(() async {
-    await _tempDir.delete(recursive: true);
-    _tempDir = null;
-  });
-
   test('validate pub completed', () async {
-    var type =
-        await FileSystemEntity.type(p.join(_tempDir.path, 'pubspec.lock'));
+    var type = await FileSystemEntity.type(p.join(d.sandbox, 'pubspec.lock'));
 
     expect(type, FileSystemEntityType.FILE);
   });
 
   test('generate VizRoot', () async {
-    VizRoot vp = await VizRoot.forDirectory(_tempDir.path);
+    VizRoot vp = await VizRoot.forDirectory(d.sandbox);
 
     expect(vp.root.name, 'test_pubspec');
     expect(vp.packages, contains('http'));
@@ -39,19 +31,16 @@ void main() {
 }
 
 Future _initTest() async {
-  assert(_tempDir == null);
-  _tempDir = await Directory.systemTemp.createTemp('pubviz.test.');
-
   // add pubspec
   var content =
       await new File(p.join('test', 'test_pubspec.yaml')).readAsString();
 
-  await new File(p.join(_tempDir.path, 'pubspec.yaml')).writeAsString(content);
+  await d.file('pubspec.yaml', content).create();
 
   // NOTE: since all dependencies in the the sample pubspec are in pubviz
   //       we can use offline to improve speed.
   ProcessResult pr = await Process.run('pub', ['get', '--offline'],
-      workingDirectory: _tempDir.path);
+      workingDirectory: d.sandbox);
 
   if (pr.exitCode != 0) {
     fail([pr.stdout, pr.stderr].join('\n'));
