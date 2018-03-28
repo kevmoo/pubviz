b9d67eac97bf292650cf272c382da8bfb960e31b
Kevin Moore <kevmoo@google.com>
2018-03-27 21:28:40 -0700

    A couple more CLI tests

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -24,4 +24,52 @@ If <package path> is omitted, the current directory is used.''');
 
     await proc.shouldExit(0);
   });
+
+  test('bad flag', () async {
+    var proc = await TestProcess.start('dart', ['bin/pubviz.dart', '--bob']);
+
+    var output = await proc.stdoutStream().join('\n');
+    expect(output, r'''Could not find an option named "bob".
+
+Usage: pubviz [--format=<format>] [--ignore-packages=<package1>,<package2>] (open | print) [<package path>]
+
+  open   Populate a temporary file with the content and open it.
+  print  Print the output to stdout.
+
+-f, --format                
+          [dot]             Generate a GraphViz dot file
+          [html]            Wrap the GraphViz dot format in an HTML template which renders it.
+
+-i, --ignore-packages       A comma seperated list of packages to exclude in the output.
+-o, --[no-]flag-outdated    Check pub.dartlang.org for lasted packages and flag those that are outdated.
+-h, --[no-]help             Print this help content.
+
+If <package path> is omitted, the current directory is used.''');
+
+    await proc.shouldExit(64);
+  });
+
+  test('no command', () async {
+    var proc = await TestProcess.start('dart', ['bin/pubviz.dart']);
+
+    var output = await proc.stdoutStream().join('\n');
+    expect(output, r'''Specify a command: open, print
+
+Usage: pubviz [--format=<format>] [--ignore-packages=<package1>,<package2>] (open | print) [<package path>]
+
+  open   Populate a temporary file with the content and open it.
+  print  Print the output to stdout.
+
+-f, --format                
+          [dot]             Generate a GraphViz dot file
+          [html]            Wrap the GraphViz dot format in an HTML template which renders it.
+
+-i, --ignore-packages       A comma seperated list of packages to exclude in the output.
+-o, --[no-]flag-outdated    Check pub.dartlang.org for lasted packages and flag those that are outdated.
+-h, --[no-]help             Print this help content.
+
+If <package path> is omitted, the current directory is used.''');
+
+    await proc.shouldExit(64);
+  });
 }
