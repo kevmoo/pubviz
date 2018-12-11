f163a14d8ad1093035cfdad2d26bd8fdd5489f50
Kevin Moore <kevmoo@google.com>
2018-12-11 15:15:59 -0800

    support final locals with latest build_cli release

--- lib/src/options.g.dart
+++ lib/src/options.g.dart
@@ -48,6 +48,6 @@ ArgParser _$populateOptionsParser(ArgParser parser) => parser
 final _$parserForOptions = _$populateOptionsParser(ArgParser());
 
 Options parseOptions(List<String> args) {
-  var result = _$parserForOptions.parse(args);
+  final result = _$parserForOptions.parse(args);
   return _$parseOptionsResult(result);
 }
