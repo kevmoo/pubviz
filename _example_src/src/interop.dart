3e9d66d0385890a0074a5497815f20c89e0a970c
kevmoo <kevmoo@google.com>
2026-04-01 23:02:04 -0700

    example: move code into classes

---
+++ _example_src/src/interop.dart
@@ -0,0 +1,33 @@
+import 'dart:js_interop';
+
+import 'package:web/web.dart';
+
+@JS()
+// ignore: non_constant_identifier_names
+external VizClass get Viz;
+
+extension type VizClass._(JSObject _) implements JSObject {
+  external JSPromise<VizInstance> instance();
+}
+
+extension type VizInstance._(JSObject _) implements JSObject {
+  external String renderString(String src, [RenderOptions options]);
+}
+
+extension type RenderOptions._(JSObject _) implements JSObject {
+  external factory RenderOptions({String format});
+
+  external String format;
+}
+
+extension NodeListExt on NodeList {
+  Iterable<Element> get elements sync* {
+    for (var i = 0; i < length; i++) {
+      yield item(i)! as Element;
+    }
+  }
+}
+
+extension NamedNodeMapExt on NamedNodeMap {
+  String? operator [](String key) => getNamedItem(key)?.value;
+}
