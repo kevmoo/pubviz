3a5e3aea519f1bcdbc45d0e26799a6453d687a83
kevmoo <kevmoo@google.com>
2026-04-10 18:57:14 -0700

    fix: drop Dart's URI logic - save a lot of space!

--- web/web_app.dart
+++ web/web_app.dart
@@ -4,12 +4,12 @@ library;
 import 'dart:js_interop';
 import 'dart:js_interop_unsafe';
 
-import 'package:web/web.dart';
+import 'package:web/web.dart' as web;
 
 import 'src/pubviz_app.dart';
 
 void main() async {
-  final url = Uri.parse(window.location.href).resolve('viz_data.js').toString();
+  final url = web.URL('viz_data.js', web.window.location.href).href;
   final module = await importModule(url.toJS).toDart;
   final vizDataString = module
       .getProperty<JSString?>('vizDataString'.toJS)
