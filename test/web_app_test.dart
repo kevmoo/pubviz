9add20d5df0f6fd3775b65bf2dfe3bcf18f5b234
Kevin Moore <kevmoo@users.noreply.github.com>
2026-04-28 17:49:55 -0700

    chore: Better web test, with latest pkg:test (#158)

--- test/web_app_test.dart
+++ test/web_app_test.dart
@@ -3,89 +3,19 @@ library;
 
 import 'dart:js_interop';
 
-import 'package:pubviz/src/web/interop.dart';
 import 'package:pubviz/src/web/pubviz_app.dart';
 import 'package:test/test.dart';
 import 'package:web/web.dart';
 
 void main() {
   setUp(() {
-    // Clean up previous test containers to ensure isolation
-    final existing = document.querySelectorAll('.test-container');
-    for (final e in existing.elements) {
-      e.remove();
-    }
+    final template =
+        document.querySelector('#test-dom-template') as HTMLTemplateElement;
 
-    final container = document.createElement('div')
-      ..className = 'test-container'
-      ..innerHTML =
-          '''
-      <input id="controlsToggle" type="checkbox">
-      <label id="hamburgerLabel"></label>
-      <input id="zoomCheckbox" type="checkbox">
-      <input id="devDependenciesCheckbox" type="checkbox">
-      <input id="workspaceOnlyCheckbox" type="checkbox">
-      <input id="hideIsolatedCheckbox" type="checkbox">
-      <div id="outdatedCheckboxContainer"></div>
-      <input id="outdatedOnlyCheckbox" type="checkbox">
-      <div id="deps-in-box"></div>
-      <div id="deps-out-box"></div>
-      <div id="toast"></div>
-      <div id="mobile-overlay"></div>
-      <span id="version"></span>
-      <div id="graph-container"></div>
-      <div id="crash-overlay" class="hidden"></div>
-      <pre id="crash-trace"></pre>
-    '''
-              .toJS;
-    document.body!.appendChild(container);
-
-    final script = document.createElement('script') as HTMLScriptElement
-      ..textContent = '''
-      window.Viz = {
-        instance: function() {
-          return Promise.resolve({
-            renderString: function(src, options) {
-              return '<svg id="mock-svg"><g class="node"><title>my_cool_package</title><ellipse stroke="#000000"></ellipse></g></svg>';
-            }
-          });
-        }
-      };
-      
-      window.Worker = function(url) {
-        this.postMessage = function(msg) {
-          setTimeout(async () => {
-            if (this.onmessage) {
-              let output = '';
-              let success = true;
-              let error = '';
-              try {
-                if (window.Viz && window.Viz.instance) {
-                  const viz = await window.Viz.instance();
-                  output = viz.renderString(msg.dotString, msg.options);
-                } else {
-                  output = '<svg id="mock-svg"><g class="node"><title>my_cool_package</title><ellipse stroke="#000000"></ellipse></g></svg>';
-                }
-              } catch (e) {
-                success = false;
-                error = e.toString();
-              }
-              this.onmessage({
-                data: {
-                  success: success,
-                  output: output,
-                  error: error,
-                  stack: '',
-                  generation: msg.generation
-                }
-              });
-            }
-          }, 0);
-        };
-        this.terminate = function() {};
-      };
-    ''';
-    document.body!.appendChild(script);
+    // Reset the DOM to a clean state
+    document.querySelector('#test-area')!
+      ..innerHTML = ''.toJS
+      ..appendChild(template.content.cloneNode(true));
   });
 
   test('PubvizApp initializes and sets document title', () {
